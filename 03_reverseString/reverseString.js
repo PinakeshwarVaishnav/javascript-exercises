0f405666dc8b2dceddb09ef68ce2abfaf2f96bdc
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2023-10-07 19:18:25 +0530

    Using for loop completed the function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    resultString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        resultString +=  str[i];
+    }
+    return resultString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
