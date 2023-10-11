7402f98fa39d0a0f731674427d076a9b498010cf
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2023-10-11 17:55:31 +0530

    feat: Implement removeFromArray function
    This commit adds a new JavaScript function, removeFromArray, which allows for the removal
    of specified elements from an array. The function utilizes the filter method and a
    callback function to create a new array with the specified values removed. It handles
    single and multiple values for removal, providing a flexible solution for array
    manipulation.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
-};
+function removeFromArray(array, ...valuesToRemove) {
+    // Use the filter method to create a new array without the values to be removed
+    const newArray = array.filter(item => !valuesToRemove.includes(item));
+    return newArray;
+  }
+  
 
 // Do not edit below this line
 module.exports = removeFromArray;
