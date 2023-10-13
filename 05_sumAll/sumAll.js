76730acea41032d1fc641941de529482db008794
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2023-10-13 18:51:08 +0530

    feat: Implement sumAll function for calculating integer sums
    This commit introduces the `sumAll` function, which calculates the sum of integers within a specified range. The function can handle both positive and negative input values, providing the correct sum. It includes input validation to address negative numbers, and clear error messages are returned for invalid inputs.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(firstInteger, secInteger) {
+    if (firstInteger < 0 || secInteger < 0) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (typeof firstInteger === "number" && typeof secInteger === "number") {
+        if (firstInteger < secInteger) {
+            for (let i = firstInteger; i <= secInteger; i++) {
+                sum += i;
+            }
+        }
+        else if (firstInteger > secInteger) {
+            for (let i = secInteger; i <= firstInteger; i++) {
+                sum += i;
+            }
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    return sum;
 };
 
 // Do not edit below this line
