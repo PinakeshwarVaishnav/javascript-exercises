de033a17d07c1fd81cb9ced2a98d65ac26092752
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2023-10-14 17:52:37 +0530

    Implement temperature conversion functions
    This commit adds two functions for temperature conversion: `convertToCelsius` and `convertToFahrenheit`. Each function takes a temperature value as input and returns the equivalent temperature in the other unit (Celsius to Fahrenheit or vice versa). The functions also round the result to one decimal place for better precision.
    
    Changes:
    - Added `convertToCelsius` and `convertToFahrenheit` functions.
    - Implemented temperature conversion logic.
    - Utilized proper rounding to one decimal place.
    - Improved code structure and variable naming for clarity.
    - Ensured function-level variable scoping for safer code.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  fahrenheitToCelsius = (fahrenheit - 32) / 1.8;
+  roundedNumber = Math.round(fahrenheitToCelsius * 10) / 10;
+  return roundedNumber;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  celsiusToFahrenheit = (celsius * 1.8) + 32;
+  roundedNumber = Math.round(celsiusToFahrenheit * 10) / 10;
+  return roundedNumber;
 };
 
 // Do not edit below this line
