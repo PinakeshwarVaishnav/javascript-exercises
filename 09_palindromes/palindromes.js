d225947b6388b25c07463800acc864d6595ef8d3
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2023-10-31 18:06:13 +0530

    feat: Implement palindromes function
    - Transform input array to lowercase and remove non-alphanumeric characters.
    - Check if the resulting string is a palindrome by comparing characters from both ends.
    - Return `true` if it's a palindrome; otherwise, return `false`.
    
    This change adds a new function, 'palindromes,' to check if a given string is a palindrome or not. The function handles input cleaning and comparison logic.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (array) {
+    array = array.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const len = array.length;
+    for (let i = 0; i < Math.floor(len / 2); i++) {
+            if (array[i] !== array[len - 1 - i]) {
+                return false;
+            }
+    }
+    return true;
 };
 
 // Do not edit below this line
