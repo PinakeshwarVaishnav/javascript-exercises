d844fb371175c3cec0d28f2064fade0b9d5e7e97
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2023-10-31 19:02:57 +0530

    Optimize Fibonacci Function and Handle Edge Cases
    Description:
    Refactored the Fibonacci function for improved performance and added handling for edge cases. The changes include:
    
    Improved type checking: Added a check to ensure that the input n is a valid number and converted it to an integer if it's a string.
    Enhanced error handling: Added a condition to return "OOPS" when the input is not a valid positive integer or when n is less than zero.
    Optimized base cases: Removed unnecessary recursive calls by directly returning 1 for Fibonacci numbers 1 and 2.
    Updated recursive logic: Optimized the Fibonacci calculation by using tail recursion.
    These changes make the code more robust and efficient.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (typeof n === "string") {
+        n = parseInt(n);
+    }
+    if (isNaN(n) || n < 0) {
+        return "OOPS";
+    }
+    if (n === 1 || n === 2) {
+        return 1;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 };
 
 // Do not edit below this line
