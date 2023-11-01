39d619d6fe23ff605c60f9d930dd59f3ee278926
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2023-11-01 17:37:17 +0530

    feat: Implement findTheOldest function
    Create the findTheOldest function to find the oldest person in an array of individuals. The function uses the current year to calculate ages, taking into account both the year of birth and, if available, the year of death for each person. This function utilizes the reduce method to determine the oldest person.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
 
+    return array.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = (oldestPerson.yearOfDeath || currentYear) - oldestPerson.yearOfBirth;
+        const currentAge = (currentPerson.yearOfDeath || currentYear) - currentPerson.yearOfBirth;
+
+        return currentAge > oldestAge ? currentPerson : oldestPerson;
+    });
 };
 
-// Do not edit below this line
 module.exports = findTheOldest;
